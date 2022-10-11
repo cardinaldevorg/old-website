fdceace0da0dcb82fb752d2fd24e9bcf7cb16f0e
Sami Elsayed <90798961+selsayed25@users.noreply.github.com>
2022-10-11 00:26:55 +0000

    fix: code formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,29 @@
 import * as React from 'react'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import { StaticImage } from 'gatsby-plugin-image'
 import Seo from '../components/seo'
 import { Link } from 'gatsby'
 
 const IndexPage = () => {
-	return (
-		<Layout pageTitle = "Home">
-			<p>Welcome to the Official Techfluent website! Here we strive to helping students who are unrepresented or disadvangted in their communities who are interested in the tech industry get the resources and passion to become the next generation of thinkers and problem-solvers.</p>
-      <center>
-      <StaticImage
-        alt = "The Official Techfluent logo"
-        src = "../images/techfluentlogo1.png"
-      />
-      </center>
-			<p>Currently we don't have anything open yet, but please stay tuned in our blog page as we will be updating on what is to come!</p>
-			<footer>
-				<h3>
-					Follow us on Social Media!
-				</h3>
-				<p>
-					Facebook: <Link to = "https://www.facebook.com/profile.php?id=100086269827506">Techfluent</Link>
-					<br />
-					Instagram: <Link to = "https://instagram.com/techfluente">@techfluent</Link>
-				</p>
-			</footer>
-    </Layout>
-	)
+   return (
+      <Layout pageTitle = "Home">
+         <p>Welcome to the Official Techfluent website! Here we strive to helping students who are unrepresented or disadvangted in their communities who are interested in the tech industry get the resources and passion to become the next generation of thinkers and problem-solvers.</p>
+         <center>
+            <StaticImage alt = "The Official Techfluent logo" src = "../images/techfluentlogo1.png" />
+         </center>
+         <p>Currently we don't have anything open yet, but please stay tuned in our blog page as we will be updating on what is to come!</p>
+         <footer>
+            <h3>Follow us on Social Media!</h3>
+            <p>
+               Facebook: <Link to = "https://www.facebook.com/profile.php?id=100086269827506">Techfluent</Link>
+               <br />
+               Instagram: <Link to = "https://instagram.com/techfluente">@techfluent</Link>
+            </p>
+         </footer>
+      </Layout>
+   )
 }
 
-export const Head = () => <Seo title = "Home Page"/>
+export const Head = () => <Seo title = "Home Page" />
 
 export default IndexPage
