90f81d7faa1e3c4f5ef30eff0f41e6b56351d01a
Sami Elsayed <90798961+selsayed25@users.noreply.github.com>
2022-10-11 00:18:37 +0000

    fix: code formatting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  	siteMetadata: {
-    	title: "Techfluent",
-			logo: "./images/techfluentlogo1.png",
-  	},
-  	plugins: [
-		`gatsby-plugin-image`,
-		`gatsby-plugin-sharp`,
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `blog`,
-				path: `${__dirname}/blog`,
-			},
-		},
-		"gatsby-plugin-mdx",
-		"gatsby-transformer-sharp",
-  	],
+   siteMetadata: {
+      title: "Techfluent",
+      logo: "./images/techfluentlogo.png",
+   },
+   plugins: [
+      `gatsby-plugin-image`,
+      `gatsby-plugin-sharp`,
+      {
+         resolve: `gatsby-source-filesystem`,
+         options: {
+            name: `blog`,
+            path: `${__direname}/blog`,
+         },
+      },
+      `gatsby-plugin-mdx`,
+      `gatsby-transformer-sharp`,
+   ],
 };
