418cfb22ab81971b416ed12d48660b191dd3c947
Sami Elsayed <2025selsayed@tjhsst.edu>
2022-10-24 13:21:42 -0400

    dep(add): new dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,15 @@ module.exports = {
             path: `${__dirname}/blog`,
          },
       },
+      {
+         resolve: `gatsby-plugin-netlify-admin`,
+         options: {
+            adminPath: `${__dirname}/src/admin`,
+            adminUri: `admin`,
+            loginUri: `/admin/login/`,
+            excludeUri: [`admin/signup`],
+         },
+      },
       `gatsby-plugin-mdx`,
       `gatsby-transformer-sharp`,
    ],
